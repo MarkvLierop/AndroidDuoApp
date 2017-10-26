64fdc38dbd8d780a55ee53c225f89f597bfe1b2b
LorensoD <lorenso.dagostino@gmail.com>
2017-10-26 14:30:15 +0200

    Filter implementation

--- AndroidDuoApp/app/src/main/java/planet/androidduoapp/MainActivity.java
+++ AndroidDuoApp/app/src/main/java/planet/androidduoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package planet.androidduoapp;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -58,6 +60,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Menuke komt nu", Toast.LENGTH_SHORT).show();
             }
         });
+
+        //Register for context menu.
+        registerForContextMenu(btnApplyFilter);
+    }
+
+    public void startFilterActivity() {
+        Intent intent = new Intent(MainActivity.this, FilterActivity.class);
+        MainActivity.this.startActivity(intent);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if(v.getId() == R.id.btnApplyFilter) {
+            this.getMenuInflater().inflate(R.menu.menu_filter, menu);
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
     }
 
     private void setupViewPager(ViewPager viewPager) {
