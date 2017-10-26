5b544a41f94fd6d6c15cb500f4932c84abf2323b
Mark <mark-vl@live.nl>
2017-10-26 15:08:19 +0200

    sdf

--- AndroidDuoApp/app/src/main/java/planet/androidduoapp/MainActivity.java
+++ AndroidDuoApp/app/src/main/java/planet/androidduoapp/MainActivity.java
@@ -3,6 +3,7 @@ package planet.androidduoapp;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import  android.support.design.widget.TabLayout;
