827d64ae50c23f5200881f841c7952f240064296
LorensoD <lorenso.dagostino@gmail.com>
2017-10-26 15:01:34 +0200

    test

--- AndroidDuoApp/app/src/main/java/planet/androidduoapp/Tab1Fragment.java
+++ AndroidDuoApp/app/src/main/java/planet/androidduoapp/Tab1Fragment.java
@@ -1,14 +1,22 @@
 package planet.androidduoapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import org.json.JSONException;
 
@@ -20,30 +28,35 @@ import classes.GoogleApi;
  * Created by Lorenso on 13-Oct-17.
  */
 
-public class Tab1Fragment extends Fragment {
+public class Tab1Fragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "Tab1Fragment";
 
-    private Button btnMap;
+    private GoogleMap mMap;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view  = inflater.inflate(R.layout.tab1_map, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        btnMap = (Button) view.findViewById(R.id.btnMap);
-        btnMap.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Ja hier dus kaart!", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-                GoogleApi ga = new GoogleApi();
-                try {
-                    ga.parseJSON();
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        // permission check.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
 
-        return view;
+        mMap.setMyLocationEnabled(true);
+
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
